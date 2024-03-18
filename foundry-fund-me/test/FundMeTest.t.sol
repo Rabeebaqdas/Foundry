e07eeceb57dc9553e28e151553410cf64fd545dc
rabeebaqdas <rabeebaqdas@gmail.com>
2024-03-18 12:42:31 +0500

    installing libraries

--- foundry-fund-me/test/FundMeTest.t.sol
+++ foundry-fund-me/test/FundMeTest.t.sol
@@ -1,9 +1,19 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../script/FundMe.sol";
+
 contract FundMeTest {
+    FundMe fundMe;
+
     function setUp() external {
-        
+        fundMe = new FundMe();
+        console.log(fundMe.MINIMUM_USD());
     }
+
+    // function testMinimum() public {
+        
+    //     assertEq(fundMe.MINIMUM_USD(), 5e18);
+    // }
 }
